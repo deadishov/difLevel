a7ea521d96e61adca7ee9b10d9dc126c46c8ac7c
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-04 23:16:43 +0300

    исправил ошибки

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let num = 266219;
+let myVar = 1
+
+
+num.toString().split('').forEach(elem => myVar *= elem);
 console.log(num);
-num = 2 * 6 * 6 * 2 * 1 * 9;
-console.log(num);
-num **= 3;
-console.log(num);
-console.log(num[0, 1]);
+console.log(myVar);
+console.log(myVar **= 3);
+console.log(myVar.toString().substring(0, 2));
