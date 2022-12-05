4a7e74a25da0ab38a52e3981cee5ceaa474cb9c0
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-06 01:55:43 +0300

    сделал первое задание усложненки

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let lang = prompt('Выберите язык/Choose language', 'ru/eng');
+
+if (lang == 'ru') {
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+} else if (lang == 'eng') {
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}
+
+
+switch (lang) {
+    case 'ru':
+        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+        break
+    case 'eng':
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+        break
+}
+
+let mass = {
+    'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    'eng': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+};
+console.log(mass[lang]);
