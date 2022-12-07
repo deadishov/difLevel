6e164b31638fc697c4b249821c523190e56aa49e
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-07 15:45:35 +0300

    Исправил ошибки

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 'use strict';
 
-const example = '            Hey dude! How are you? How is everything?                   ';
+const example = '                  Hey dude? How are you? How is everything?';
 
 
 
 function someFunc(freeArg) {
     if (typeof freeArg != 'string') {
         console.log('Аргумент не является строкой');
+    } else if (typeof freeArg == 'string' && freeArg.length < 30) {
+        console.log(freeArg.trim());
+    } else if (typeof freeArg == 'string' && freeArg.length > 30) {
+        console.log(freeArg.trim().slice(0, 29) + '...');
     }
-
-    console.log(freeArg.trim().slice(0, 29) + '...');
 }
-
 
 
 someFunc(example);
