d10bb93548a39268719292a3e14b351caa24da81
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-08 15:44:23 +0300

    Выполнил усложненное 2

--- script.js
+++ script.js
@@ -21,3 +21,30 @@ screenPrice = asking();
 
 
 console.log(screenPrice);
+
+
+// Усложненное задание №2
+
+let x = 100
+
+
+
+const arr = ['23124', '345463', '56745', '4785646', '3222344', '23457', '56656556']
+
+for (let i = 0; i < 7; i++) {
+    if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
+        console.log(arr[i]);
+    }
+}
+
+nextNumber:
+for (let i = 2; i <= x; i++) {
+
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+            continue nextNumber;
+        }
+    }
+    console.log(i + 'Делители этого числа ' + i);
+}
+
