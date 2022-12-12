5f753af945e2b98ba4786e7cb31869d983c96290
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-12 14:40:48 +0300

    выполнено

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+
+    let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+    let date = new Date();
+
+    week.forEach(function (day, index) {
+        let days = document.createElement('div');
+        days.innerHTML = day;
+        document.body.appendChild(days);
+
+        if (index === 5 || index === 6) {
+            days.style.fontStyle = 'italic';
+        }
+        let currentDay = date.getDay();
+        if (currentDay === 0) {
+            currentDay = 7;
+        }
+        if (index === (currentDay - 1)) {
+            days.style.fontWeight = 'bold';
+        }
+    });
+});
